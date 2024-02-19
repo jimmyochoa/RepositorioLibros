ae273cd686847f4a5a08ba04adc3183d43c11af0
danieee5 <137199641+danieee5@users.noreply.github.com>
2024-02-19 16:26:10 -0500

    Update dashboard.blade.php
    Actualizar el dashboard lol

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,25 @@
     <!--Cdn Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+
+    <!--Google Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Lato', sans-serif;
+                font-style: italic; /* Todos los textos en Lato cursiva, excepto los títulos */
+            }
+    
+            .card-header h3 {
+                font-family: 'Montserrat', sans-serif;
+                font-weight: 700;
+                font-style: normal; /* Montserrat normal para títulos */
+            }
+        </style>
+
+    </head>
 
 <body>
     <div class="container mt-5">
@@ -21,7 +39,6 @@
                     <div class="card-body">
                         <p>Bienvenido, {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</p>
                         <p>Tu dirección de correo electrónico es: {{ Auth::user()->email }}</p>
-                        <!-- Aquí puedes agregar más detalles sobre el usuario -->
                     </div>
                     <div class="card-footer text-center">
                         <form action="{{ route('logout') }}" method="POST">
@@ -36,3 +53,4 @@
 </body>
 
 </html>
+
